8a97164c5465835092745177b1e07853dc342fe5
Rehan <ranarehan9x@gmail.com>
2025-04-19 14:53:43 +0500

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export default {
     baseUrl: 'https://srv694651.hstgr.cloud/drivebidapi',
-    chatBaseUrl: 'https://srv694651.hstgr.cloud/socket.io/drivebidmessages',
-    bidBaseUrl: 'https://srv694651.hstgr.cloud/socket.io/drivebidbidding',
+    chatBaseUrl: 'https://srv694651.hstgr.cloud/drivebidapiV2',
+    bidBaseUrl: 'https://srv694651.hstgr.cloud/drivebidapiV1',
 };
